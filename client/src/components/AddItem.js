06cf814fde9fcd38eb1f72f5485837199275cd05
F-Carroll <fiachracarroll2013@gmail.com>
2021-04-29 21:18:44 +0100

    Styled the Components, AddItem & AddLocation

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,15 +1,14 @@
-import React, {useCallback, useEffect, useState} from "react";
-import {withFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { withFormik } from "formik";
 import * as Yup from "yup";
 import options from "../options.json";
 import Select from "react-select";
 
 export default function AddItem() {
-  const [items, setItems] = useState()
- const [existingBarcodes, setexistingBarcodes] = useState([])
- const [existingProducts, setexistingProducts] = useState([])
+  const [existingBarcodes, setexistingBarcodes] = useState([]);
+  const [existingProducts, setexistingProducts] = useState([]);
 
-  const selectoptions = []
+  const selectoptions = [];
 
   function Capitalize(string) {
     const input = string.toLowerCase();
@@ -26,15 +25,25 @@ export default function AddItem() {
     selectoptions.push({ value: option, label: Capitalize(option) });
   });
 
+  function handleClose() {
+    window.location = "/";
+  }
+
   const getItems = async () => {
     try {
       const response = await fetch("http://localhost:5000/api/items");
       const jsonData = await response.json();
-      setItems(jsonData);
 
-      setexistingBarcodes(jsonData.map((item) => {return item.barcode}))
-      setexistingProducts(jsonData.map((item) => {return item.product_name}))
- 
+      setexistingBarcodes(
+        jsonData.map((item) => {
+          return item.barcode;
+        })
+      );
+      setexistingProducts(
+        jsonData.map((item) => {
+          return item.product_name;
+        })
+      );
     } catch (error) {
       console.error(error.message);
     }
@@ -47,150 +56,225 @@ export default function AddItem() {
   const formikEnhancer = withFormik({
     validationSchema: Yup.object().shape({
       productname: Yup.string()
-      .min(3, "Product name is too short")
-      .max(100, "Product name is too long")
-      .notOneOf(existingProducts, 'This product already exists')
-      .required("Required"),
+        .min(3, "Product name is too short")
+        .max(100, "Product name is too long")
+        .notOneOf(existingProducts, "This product already exists")
+        .required("Required"),
 
       barcode: Yup.string()
-      .length(13, "Invalid Barcode")
-      .notOneOf(existingBarcodes, 'This barcode is taken')
-      .required("Required"),
+        .length(13, "Invalid Barcode")
+        .notOneOf(existingBarcodes, "This barcode is taken")
+        .required("Required"),
 
-            select: Yup.array().required('Required').min(1, 'Required').nullable()
+      select: Yup.array().required("Required").min(1, "Required").nullable(),
     }),
     mapPropsToValues: () => ({
       productname: "",
       barcode: "",
-      select: null
+      select: null,
     }),
     handleSubmit: async (values, { setSubmitting }) => {
-   
       const payload = {
         product_name: values.productname,
         barcode: values.barcode,
         items: values.select.map((select) => {
-          return select.value
-        })
+          return select.value;
+        }),
       };
-      try {  
-          await fetch("http://localhost:5000/api/items", {
-              method:'POST',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify(payload)
-          });
-          window.location = "/";
+      try {
+        await fetch("http://localhost:5000/api/items", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        });
+        window.location = "/";
       } catch (error) {
-          console.error(error.message)
-      }  
+        console.error(error.message);
+      }
 
       setTimeout(() => {
-        alert(JSON.stringify(payload, null, 2));
+        alert('Timed Out');
         setSubmitting(false);
       }, 1000);
     },
-    displayName: "MyForm"
+    displayName: "MyForm",
   });
+
+  const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      background: "#F3F4F6",
+      borderRadius: "8px",
+      minHeight: "40px",
+      cursor: "pointer",
+      borderColor: "none",
+      borderWidth: "2px",
+      width: "100%",
+// eslint-disable-next-line
+      borderColor: state.isFocused ? null : "#F3F4F6",
+// eslint-disable-next-line
+      background: state.isFocused ? "white" : "transition #F3F4F6",
+      boxShadow: state.isFocused ? null : null,
+    }),
+  };
 
   const MyForm = (props) => {
     const {
       values,
       touched,
-      dirty,
       errors,
       handleChange,
       handleBlur,
       handleSubmit,
-      handleReset,
       setFieldValue,
       setFieldTouched,
-      isSubmitting
+      isSubmitting,
     } = props;
     return (
-      <form onSubmit={handleSubmit}>
-        <input
-          id="productname"
-          type="text"
-          placeholder="Enter Product Name"
-          value={values.productname}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.productname && touched.productname && (
-          <div style={{ color: "red", marginTop: ".5rem" }}>{errors.productname}</div>
-        )}
-
-        <input
-          id="barcode"
-          type="number"
-          placeholder="Enter barcode"
-          value={values.barcode}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.barcode && touched.barcode && (
-          <div id="barcode-err"style={{ color: "red", marginTop: ".5rem" }}>{errors.barcode}</div>
-        )}
-        <MySelect
-          value={values.select}
-          onChange={setFieldValue}
-          onBlur={setFieldTouched}
-          error={errors.select}
-          touched={touched.select}
-        />
-
-{touched.select && errors.select ? errors.select  &&
- <div style={{ color: "red", marginTop: ".5rem" }}>{errors.select}</div>
-: '' }
-              
-                <button
-          type="button"
-          className="outline"
-          onClick={handleReset}
-          disabled={!dirty || isSubmitting}
+      <div className="h-screen80 relative">
+        <div className="flex flex-row justify-between">
+          <h1 className="text-4xl p-7 font-semibold">Add Item</h1>
+          <button
+            className="self-center p-3 mr-6 bg-gray-200 rounded-md hover:bg-gray-300"
+            onClick={handleClose}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center "
         >
-          Reset
-        </button>
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-  
-        
-      </form>
+          <div className="w-2/3 sm:w-2/5 m-2.5 h-20">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1.5"
+            >
+              Product Name
+            </label>
+            <input
+              className="bg-gray-100 appearance-none border-2 rounded-md w-full border-gray-100 p-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-300"
+              id="productname"
+              type="text"
+              autoComplete="off"
+              placeholder="Enter Product Name"
+              value={values.productname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.productname && touched.productname && (
+              <div className="text-red-500 p-1 text-sm">
+                {errors.productname}
+              </div>
+            )}
+          </div>
+
+          <div className="w-2/3 sm:w-2/5 m-2.5 h-20">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1.5"
+            >
+              Barcode
+            </label>
+            <input
+              className="bg-gray-100 appearance-none w-full border-2 rounded-md border-gray-100 p-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-300"
+              id="barcode"
+              type="number"
+              placeholder="Enter Barcode"
+              autoComplete="off"
+              value={values.barcode}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.barcode && touched.barcode && (
+              <div className="text-red-500 p-1 text-sm">{errors.barcode}</div>
+            )}
+          </div>
+          <div className="w-2/3 sm:w-2/5 m-2.5 h-20">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1.5"
+            >
+              Materials
+            </label>
+            <MySelect
+              value={values.select}
+              onChange={setFieldValue}
+              onBlur={setFieldTouched}
+              error={errors.select}
+              touched={touched.select}
+            />
+            {touched.select && errors.select
+              ? errors.select && (
+                  <div className="text-red-500 p-1 text-sm ">
+                    {errors.select}
+                  </div>
+                )
+              : ""}
+          </div>
+          <div className="w-full absolute bottom-0 text-center">
+            <button
+              className="transition duration-300  ease-in-out w-2/3 self-center sm:w-2/5 bg-blue-500 p-2 rounded-md text-base font-semibold text-white hover:bg-blue-600 disabled:bg-blue-900"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Add Item
+            </button>
+          </div>
+        </form>
+      </div>
     );
   };
-  
-  
+
   class MySelect extends React.Component {
     handleChange = (value) => {
       this.props.onChange("select", value);
     };
-  
+
     handleBlur = () => {
       this.props.onBlur("select", true);
     };
-  
+
     render() {
       return (
-        <div style={{ margin: "1rem 0" }}>
-          <label htmlFor="color">select</label>
-          <Select
-            id="color"
-            options={selectoptions}
-            onChange={this.handleChange}
-            onBlur={this.handleBlur}
-            value={this.props.value}
-            isMulti
-            backspaceRemovesValue
-            closeMenuOnSelect={false}
-          />
-        </div>
+        <Select
+          id="color"
+          placeholder="Select Materials"
+          options={selectoptions}
+          styles={customStyles}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+          isMulti
+          backspaceRemovesValue
+          closeMenuOnSelect={false}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              neutral0: "#f3f4f6",
+              primary: "#d1d5db",
+              neutral50: "#9ea6b2",
+            },
+          })}
+        />
       );
     }
   }
-  
+
   const MyEnhancedForm = formikEnhancer(MyForm);
-  
+
   return (
     <>
       <MyEnhancedForm />
--- client/src/components/AddLocation.js
+++ client/src/components/AddLocation.js
@@ -1,14 +1,13 @@
-import React,{useState, useEffect} from "react";
-import {withFormik } from "formik";
+import React, { useState, useEffect } from "react";
+import { withFormik } from "formik";
 import * as Yup from "yup";
 import options from "../options.json";
 import Select from "react-select";
 
 export default function AddLocation() {
-  const [items, setItems] = useState([])
-  const [existingLocation, setexistingLocation] = useState([])
+  const [existingLocation, setexistingLocation] = useState([]);
 
-  const selectoptions = []
+  const selectoptions = [];
 
   function Capitalize(string) {
     const input = string.toLowerCase();
@@ -25,14 +24,20 @@ export default function AddLocation() {
     selectoptions.push({ value: option, label: Capitalize(option) });
   });
 
+  function handleClose() {
+    window.location = "/";
+  }
+
   const getItems = async () => {
     try {
       const response = await fetch("http://localhost:5000/api/locations");
       const jsonData = await response.json();
-      setItems(jsonData);
 
-      setexistingLocation(jsonData.map((item) => {return item.location_name}))
- 
+      setexistingLocation(
+        jsonData.map((item) => {
+          return item.location_name;
+        })
+      );
     } catch (error) {
       console.error(error.message);
     }
@@ -45,130 +50,201 @@ export default function AddLocation() {
   const formikEnhancer = withFormik({
     validationSchema: Yup.object().shape({
       locationname: Yup.string()
-      .min(3, "Location name is too short")
-      .max(100, "Location name is too long")
-      .notOneOf(existingLocation, "This location already exists")
-      .required("Required"),
+        .min(3, "Location name is too short")
+        .max(100, "Location name is too long")
+        .notOneOf(existingLocation, "This location already exists")
+        .required("Required"),
 
-            select: Yup.array().required('Required').min(1, 'Required').nullable()
+      select: Yup.array().required("Required").min(1, "Required").nullable(),
     }),
     mapPropsToValues: () => ({
       locationname: "",
-      select: null
+      select: null,
     }),
     handleSubmit: async (values, { setSubmitting }) => {
-   
       const payload = {
         location_name: values.locationname,
         location_materials: values.select.map((select) => {
-          return select.value
-        })
+          return select.value;
+        }),
       };
-      try {  
-          await fetch("http://localhost:5000/api/locations", {
-              method:'POST',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify(payload)
-          });
-          window.location = "/";
+      try {
+        await fetch("http://localhost:5000/api/locations", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        });
+        window.location = "/";
       } catch (error) {
-          console.error(error.message)
-      }  
+        console.error(error.message);
+      }
 
       setTimeout(() => {
-        alert(JSON.stringify(payload, null, 2));
+        alert('Timed Out')
         setSubmitting(false);
       }, 1000);
     },
-    displayName: "MyForm"
+    displayName: "MyForm",
   });
+
+  const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      background: "#F3F4F6",
+      borderRadius: "8px",
+      minHeight: "40px",
+      cursor: "pointer",
+      borderColor: "none",
+      borderWidth: "2px",
+      width: "100%",
+  // eslint-disable-next-line
+      borderColor: state.isFocused ? null : "#F3F4F6",
+  // eslint-disable-next-line
+      background: state.isFocused ? "white" : "transition #F3F4F6",
+      boxShadow: state.isFocused ? null : null,
+    }),
+  };
 
   const MyForm = (props) => {
     const {
       values,
       touched,
-      dirty,
       errors,
       handleChange,
       handleBlur,
       handleSubmit,
-      handleReset,
       setFieldValue,
       setFieldTouched,
-      isSubmitting
+      isSubmitting,
     } = props;
     return (
-      <form onSubmit={handleSubmit}>
-        <input
-          id="locationname"
-          type="text"
-          placeholder="Enter Location Name"
-          value={values.locationname}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.locationname && touched.locationname && (
-          <div style={{ color: "red", marginTop: ".5rem" }}>{errors.locationname}</div>
-        )}
-
-        <MySelect
-          value={values.select}
-          onChange={setFieldValue}
-          onBlur={setFieldTouched}
-          error={errors.select}
-          touched={touched.select}
-        />
-
-{touched.select && errors.select ? errors.select  &&
- <div style={{ color: "red", marginTop: ".5rem" }}>{errors.select}</div>
-: '' }
-              
-                <button
-          type="button"
-          className="outline"
-          onClick={handleReset}
-          disabled={!dirty || isSubmitting}
+      <div className="h-screen80 relative">
+        <div className="flex flex-row justify-between">
+          <h1 className="text-4xl p-5 self-center font-semibold">
+            Add Location
+          </h1>
+          <button
+            className="self-center p-3 mr-6 bg-gray-200 rounded-md hover:bg-gray-300"
+            onClick={handleClose}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center"
         >
-          Reset
-        </button>
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-      </form>
+          <div className="w-2/3 sm:w-2/5 m-2.5 h-20">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1.5"
+            >
+              Location Name
+            </label>
+            <input
+              className="bg-gray-100 appearance-none border-2 rounded-md w-full border-gray-100 p-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-300"
+              id="locationname"
+              type="text"
+              autoComplete="off"
+              placeholder="Enter Location Name"
+              value={values.locationname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.locationname && touched.locationname && (
+              <div className="text-red-500 p-1 text-sm">
+                {errors.locationname}
+              </div>
+            )}
+          </div>
+
+          <div className="w-2/3 sm:w-2/5 m-2.5 h-20 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1.5"
+            >
+              Materials
+            </label>
+            <MySelect
+              value={values.select}
+              onChange={setFieldValue}
+              onBlur={setFieldTouched}
+              error={errors.select}
+              touched={touched.select}
+            />
+
+            {touched.select && errors.select
+              ? errors.select && (
+                  <div className="text-red-500 p-1 text-sm">
+                    {errors.select}
+                  </div>
+                )
+              : ""}
+          </div>
+          <div className="h-20" />
+          <div className="w-full absolute bottom-0 text-center">
+            <button
+              type="submit"
+              className="transition duration-300  ease-in-out w-2/3 sm:w-2/5 bg-blue-500 p-2 rounded-md text-base font-semibold text-white hover:bg-blue-600 disabled:bg-blue-900"
+              disabled={isSubmitting}
+            >
+              Add Location
+            </button>
+          </div>
+        </form>
+      </div>
     );
   };
-  
-  
+
   class MySelect extends React.Component {
     handleChange = (value) => {
       this.props.onChange("select", value);
     };
-  
+
     handleBlur = () => {
       this.props.onBlur("select", true);
     };
-  
+
     render() {
       return (
-        <div style={{ margin: "1rem 0" }}>
-          <label htmlFor="color">Select Recyclable Materials at this Location</label>
-          <Select
-            id="color"
-            options={selectoptions}
-            onChange={this.handleChange}
-            onBlur={this.handleBlur}
-            value={this.props.value}
-            isMulti
-            backspaceRemovesValue
-            closeMenuOnSelect={false}
-          />
-        </div>
+        <Select
+          id="color"
+          options={selectoptions}
+          styles={customStyles}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+          isMulti
+          backspaceRemovesValue
+          closeMenuOnSelect={false}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              neutral0: "#f3f4f6",
+              neutral50: "#9ea6b2",
+              primary: "#d1d5db",
+            },
+          })}
+        />
       );
     }
   }
-  
+
   const MyEnhancedForm = formikEnhancer(MyForm);
-  
+
   return (
     <>
       <MyEnhancedForm />
