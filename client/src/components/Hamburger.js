709bca0e47abc585d1426637c249ddd123994b6c
F-Carroll <fiachracarroll2013@gmail.com>
2021-05-01 20:24:16 +0100

    Navbar Redesign

---
+++ client/src/components/Hamburger.js
@@ -0,0 +1,178 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+export default function Hamburger() {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        {({ open }) => (
+          <>
+            <div>
+              <Menu.Button className="inline-flex justify-center w-full p-2 text-sm font-medium text-black bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                <HamburgerIcon />
+              </Menu.Button>
+            </div>
+            <Transition
+              show={open}
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items
+                static
+                className="absolute right-0 w-48 mt-2 origin-top-right bg-gray-50 divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-black" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        onClick={openAddItem}
+                      >
+                        <AddItemIcon
+                          className="w-5 h-5 pr-2"
+                          aria-hidden="true"
+                        />
+                        Add Item
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-black" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        onClick={openAddLocation}
+                      >
+                        <AddLocationIcon
+                          className="w-5 h-5"
+                          aria-hidden="true"
+                        />
+                        Add Location
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="px-1 py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-gray-100 text-black" : "text-gray-900"
+                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        onClick={openSettings}
+                      >
+                        <SettingsIcon className="w-5 h-5" aria-hidden="true" />
+                        Settings
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
+      </Menu>
+    </div>
+  );
+}
+
+function HamburgerIcon(props) {
+  return (
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      stroke="#3A2E39"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+}
+function AddItemIcon(props) {
+  return (
+    <svg
+      className="w-5 h-5 mr-2"
+      fill="none"
+      stroke="#60D394"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
+      />
+    </svg>
+  );
+}
+
+function AddLocationIcon(props) {
+  return (
+    <svg
+      className="w-5 h-5 mr-2"
+      fill="none"
+      stroke="#60D394"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  );
+}
+
+function SettingsIcon(props) {
+  return (
+    <svg
+      className="w-5 h-5 mr-2"
+      fill="none"
+      stroke="#60D394"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+      />
+    </svg>
+  );
+}
+
+function openAddItem() {
+  window.location = "/add/item";
+}
+
+function openAddLocation() {
+  window.location = "/add/location";
+}
+
+function openSettings() {
+  window.location = "/settings";
+}
--- client/src/components/SearchInput.js
+++ client/src/components/SearchInput.js
@@ -1,11 +1,4 @@
 import React from "react";
-import { SearchIcon } from "@chakra-ui/icons";
-import {
-  Center,
-  Input,
-  InputGroup,
-  InputLeftElement,
-} from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { setQuery } from "../actions";
 
@@ -20,21 +13,30 @@ export default function SearchInput() {
 
   return (
     <>
-      <Center w="100%">
-        <InputGroup ml="10px">
-          <InputLeftElement children={<SearchIcon color="gray.500" />} />
-          <Input
-            size="md"
-            variant="filled"
-            id="search"
-            autoComplete="off"
-            placeholder="Search by EAN or Product Name"
-            type="text"
-            value={query}
-            onChange={handleOnSearch}
+      <div className="w-3/5 sm:w-2/5 relative flex items-center  text-gray-400  focus-within:text-gray-700 transition duration-300 ease-in-out ">
+        <input
+          className="w-full bg-gray-100 p-2 rounded-md pl-10 focus:outline-none"
+          autoComplete="off"
+          placeholder="Search by barcode or product"
+          type="text"
+          value={query}
+          onChange={handleOnSearch}
+        />
+        <svg
+          className="w-5 h-5 absolute left-0 mx-3"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           />
-        </InputGroup>
-      </Center>
+        </svg>
+      </div>
     </>
   );
 }
