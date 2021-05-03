8fd5a7fb4cefdd3eccfe7da082a201fa35d40a8a
F-Carroll <fiachracarroll2013@gmail.com>
2021-05-03 21:29:22 +0100

    Finished Search Result Styling

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -3,102 +3,128 @@ import { v4 as uuidv4 } from "uuid";
 import Fuse from "fuse.js";
 import { useDispatch, useSelector } from "react-redux";
 import { setItems } from "../actions";
-import { Box, Text, Container, Center, Wrap, WrapItem, Tooltip} from "@chakra-ui/react";
-import moment from 'moment'
 
 const SearchResult = (props) => {
   const dispatch = useDispatch();
   const query = useSelector((state) => state.query);
   const items = useSelector((state) => state.items);
   const selectedLocation = useSelector((state) => state.selectedLocation);
-  
+
   function Capitalize(string) {
     const input = string.toLowerCase();
     const words = input.split(" ");
 
-for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-}
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
 
-return words.join(" ")
+    return words.join(" ");
   }
-  
+
   useEffect(() => {
     const getItems = async () => {
       try {
         const response = await fetch("http://localhost:5000/api/items");
         const jsonData = await response.json();
-  
+
         dispatch(setItems(jsonData));
       } catch (error) {
         console.error(error.message);
       }
     };
-  
+
     getItems();
-    // document.getElementById("search").focus();
-  },[dispatch]);
+    document.getElementById("search").focus();
+  }, [dispatch]);
 
   const fuse = new Fuse(items, {
+    useExtendedSearch: true,
     keys: ["barcode", "product_name"],
     includeScore: true,
   });
 
-  const results = fuse.search(query);
+  const results = fuse.search("'" + query);
 
-  const itemResults = query ? results.map((result) => result.item) : items;
+  const itemResults = results.map((result) => result.item)
 
   var arr2 = selectedLocation[0];
 
   function RenderList({ items }) {
     if (items?.length) {
-      return items.map((item) => <p key={uuidv4()}>{Capitalize(item)}</p>);
+      return (
+        <div className="h-9/12 ">
+          {items.map((item) => (
+            <p key={uuidv4()}>{Capitalize(item)}</p>
+          ))}
+        </div>
+      );
     } else {
-      return <Center h="60%" color="gray.500">No items</Center>
+      return (
+        <div className="flex h-7/12 text-gray-400">
+          <p className="m-auto">No items</p>
+        </div>
+      );
     }
   }
 
   return (
-    <>
+    <div className="flex flex-col">
       {itemResults.map((item) => {
         const RecycleList = item.items.filter((item) => arr2.includes(item));
         const WasteList = item.items.filter((item) => !arr2.includes(item));
 
         return (
-<Box key={item.product_id} p="12px" pt="5px" m="30px" boxShadow="lg" borderTop="4px" borderColor="#39F969">
- <Text as="h1" fontSize="25px">{item.product_name}</Text>
- <Text as="h2" fontSize="18px" className="mb-0">{item.barcode}</Text>
-<Wrap spacing="0" my="20px">
-  <WrapItem w={["100%","50%"]}  borderRight={["0px","2px"]} borderColor={["gray.200","gray.200"]}>
-    <Container w="100%" textAlign="center" height="100%" mb={["40px","0", ]}>
-      <Center mb="3px">
-      <Tooltip hasArrow label="Recyclable" color="white" placement="top">
-            <svg height="35px" className="w-6 h-6" fill="none" stroke="#39F969" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.4" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-      </Tooltip>
-      </Center>
-      <RenderList items={RecycleList}/>
-    </Container>
-  </WrapItem>
+          <div
+            key={item.product_id}
+            className="bg-gray-100 w-11/12 md:w-3/5 my-3 p-2 self-center rounded-md border-t-4 border-primary-green "
+          >
+            <div className="h-3/12">
+              <h1 className="pl-2 pt-1 text-xl font-semibold">
+                {item.product_name}
+              </h1>
+              <h2 className="pl-2">{item.barcode}</h2>
+            </div>
+            <div className="flex flex-row h-9/12 pb-2 mt-2">
+              <div className="w-1/2 border-r-2 text-center">
+                <svg
+                  className="w-8 h-8 block m-auto mb-1.5"
+                  fill="none"
+                  stroke="#60D394"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <RenderList items={RecycleList} />
+              </div>
+              <div className="w-1/2 text-center">
+                <svg
+                  className="w-8 h-8 block m-auto mb-1.5"
+                  fill="none"
+                  stroke="#EE6055"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
 
-  <WrapItem w={["100%","50%"]}>
-  <Container w="100%" textAlign="center">
-    <Center mb="3px">
-    <Tooltip hasArrow label="Not Recyclable" color="white" placement="top">
-    <svg height="35px" className="w-6 h-6" fill="none" stroke="#E65C32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinejoin="round" strokeWidth="1.4" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    </Tooltip>
-    </Center>
-        <RenderList items={WasteList}/>
-    </Container>
-  </WrapItem>
-</Wrap>
-<Tooltip hasArrow label={moment(item.record_date).format("Do MMM YYYY [at] hh:mm A")} color="white" bg="gray.700" fontSize="9px" >
-<Text color="gray.400" fontSize="14px" mt="10px" width="fit-content" >Edited {moment(item.record_date).fromNow()}</Text>
-</Tooltip>
-</Box>
-        
+                <RenderList items={WasteList} />
+              </div>
+            </div>
+          </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
