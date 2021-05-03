c89b5360b6c2cd93fa55d41635435393f6033d72
F-Carroll <fiachracarroll2013@gmail.com>
2021-05-03 21:35:38 +0100

    Added Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./assets/main.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 //Components
 import Navbar from "./components/Navbar";
@@ -9,14 +10,15 @@ import Settings from "./components/Settings"
 import SettingsHandler from "./components/SettingsHandler"
 import AddItem from "./components/AddItem";
 import AddLocation from "./components/AddLocation";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-  
+  const query = useSelector((state) => state.query);
   return (
       <Router>
          <Route exact path="/">
            <Navbar />
-           
+           {query ? null : <LandingPage/>}
            <SearchResult/>
            <SettingsHandler/>
            </Route>
